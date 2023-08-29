adba87d1ec91f5dc8c195d73f9326fe0d9af447b
Eduardo Matheus <eduardomatheusuesc@gmail.com>
2023-08-29 16:41:32 -0300

    feat/eduardo: terminando exercicio 2 da pratica 05

--- src/atividade_pratica_05/pratica_05_Eduardo/exercicio02.cpp
+++ src/atividade_pratica_05/pratica_05_Eduardo/exercicio02.cpp
@@ -24,7 +24,52 @@ int main(void){
     Resposta: Sim fornece eu ja usei na questao anterior atraves da biblioteca cctype
     */
 
-    ch2 = '81';
-    cout << "Imprimindo o caracter ch2 em decimal: " << static_cast<int>(ch2) << endl;
+    //Letra e
+    ch2 = 81;
+    cout << "Imprimindo o caracter ch2: " << ch2 << endl;
+    cout 
+    << "Imprimindo o caracter ch2 em decimal: " 
+    << static_cast<int>(ch2) 
+    << endl;
+
+    cout 
+    << fixed
+    << oct
+    << "Imprimindo o caracter ch2 em octal: " 
+    << static_cast<int>(ch2) 
+    << endl;
+
+    cout 
+    << fixed
+    << hex
+    << "Imprimindo o caracter ch2 em hexadecimal: " 
+    << "0x"
+    << static_cast<int>(ch2) 
+    << endl;
+
+    //Letra f
+    ch3 = tolower(ch2);
+    cout << "Imprimindo o caracter ch3: " << ch3 << endl;
+    cout 
+    << "Imprimindo o caracter ch3 em decimal: " 
+    << static_cast<int>(ch3) 
+    << endl;
+
+    cout 
+    << fixed
+    << oct
+    << "Imprimindo o caracter ch3 em octal: " 
+    << static_cast<int>(ch3) 
+    << endl;
+
+    cout 
+    << fixed
+    << hex
+    << "Imprimindo o caracter ch3 em hexadecimal: " 
+    << "0x"
+    << static_cast<int>(ch3) 
+    << endl;
+
+
     return 0;
 }
