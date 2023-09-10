5dc3ec54ebbefc3108a4efff144df3c4e9acc5bf
Eduardo Matheus <eduardomatheusuesc@gmail.com>
2023-09-10 11:42:33 -0300

    Terminando exercicio 05 da pratica PI-P0007

--- src/Eduardo/PI-P0007/funcoes/funcoes_gerais.cpp
+++ src/Eduardo/PI-P0007/funcoes/funcoes_gerais.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
 // Função geral
@@ -71,13 +74,13 @@ void imprimePrimo(int numero)
 int verificaDivisores(int numero)
 {
     int cont = 0;
-    for (int i = 1; i < (numero/2); i++)
+    for (int i = 1; i < (numero / 2); i++)
     {
         numero % i == 0 ? cont++ : cont = cont;
-        if(cont > 1) return 0;
+        if (cont > 1)
+            return 0;
     }
     return cont;
-    
 }
 
 void verificaPrimo()
@@ -88,7 +91,99 @@ void verificaPrimo()
     imprimePrimo(3);
     for (int i = 5; i < 100; i += 2)
     {
-        if(verificaDivisores(i) == 1) imprimePrimo(i);
+        if (verificaDivisores(i) == 1)
+            imprimePrimo(i);
     }
-    
+}
+
+// Exercicio 05
+
+void jogoAdivinhacao()
+{
+    const string RESET_COLOR = "\033[0m";
+    const string ORANJE_TEXT = "\033[38;5;208m";
+    const string BLUE_TEXT = "\033[38;5;33m";
+    int aleatorio, escolhaUsuario;
+    srand(time(0));
+    aleatorio = (1 + rand() % 100);
+
+    do
+    {
+
+        try
+        {
+            cout << "Insira um número inteiro entre 1 e 100: ";
+            cin >> escolhaUsuario;
+            cout << endl;
+
+            if (escolhaUsuario < 1 || escolhaUsuario > 100)
+            {
+                throw out_of_range("Número fora do intervalo [1, 100]");
+            }
+
+            if (escolhaUsuario < aleatorio)
+            {
+                cout << BLUE_TEXT
+                     << "O número inserido esta abaixo do número correto !!"
+                     << RESET_COLOR
+                     << endl;
+            }
+            else if (escolhaUsuario > aleatorio)
+            {
+                cout << ORANJE_TEXT
+                     << "O número inserido esta acima do número correto !!" << RESET_COLOR
+                     << endl;
+            }
+            else
+            {
+                cout << "O número inserido esta correto !!"
+                     << endl;
+            }
+        }
+        catch (const invalid_argument &e)
+        {
+            cerr << "Erro de argumento: " << e.what() << endl;
+        }
+        catch (const out_of_range &e)
+        {
+            cerr << "Erro de intervalo: " << e.what() << endl;
+        }
+
+    } while (escolhaUsuario != aleatorio);
+}
+
+void menuJogo()
+{
+    char escolha;
+    const string RESET_COLOR = "\033[0m";
+    const string YELLOW_TEXT = "\033[33m";
+    const string BLUE_TEXT = "\033[34m";
+    const string GREEN_TEXT = "\033[32m";
+    const string RED_TEXT = "\033[31m";
+    do
+    {
+        cout << YELLOW_TEXT << "_________________________________________________________ \n\n"
+             << endl;
+        cout << "Menu do jogo:" << RESET_COLOR << endl;
+
+        cout << GREEN_TEXT << "1. Iniciar o jogo" << RESET_COLOR;
+        cout << endl;
+
+        cout << RED_TEXT << "0. Sair do jogo" << RESET_COLOR << endl;
+        cout << "Informe sua escolha: ";
+        cin >> escolha;
+        cout << endl;
+
+        int opcao = escolha - '0';
+
+        if (opcao == 1)
+        {
+            jogoAdivinhacao();
+        }
+        else if (escolha != '0')
+        {
+            cout << RED_TEXT << "Escolha inválida. Por favor, tente novamente." << RESET_COLOR << endl;
+        }
+
+    } while (escolha != '0');
 }
