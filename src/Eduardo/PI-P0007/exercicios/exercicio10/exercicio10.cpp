ab7063bab07eb0aaf98d2820398738e056609369
Eduardo Matheus <eduardomatheusuesc@gmail.com>
2023-09-24 11:43:11 -0300

    Terminando exercicio 10 da pi0007 e corrigindo erro de tipo no menu

--- src/Eduardo/PI-P0007/exercicios/exercicio10/exercicio10.cpp
+++ src/Eduardo/PI-P0007/exercicios/exercicio10/exercicio10.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include "../../funcoes/head_funcoes.h"
 
 using namespace std;
 
 void exercicio10()
 {
-    cout << "OLA MUNDO !!" << endl;
+    int num;
+
+    cout << "Digite o numero de termos da sequencia de Fibonacci a serem exibidos: ";
+    cin >> num;
+
+    if (num < 1) {
+        cout << "Por favor, digite um numero maior ou igual a 1." << endl;
+    } else {
+        imprimeSequenciaFib(num);
+    }
 }
