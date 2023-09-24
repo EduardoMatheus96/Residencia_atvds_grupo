425ea85e0c276e71f6ef85ef1f124ea677538b1a
Eduardo Matheus <eduardomatheusuesc@gmail.com>
2023-09-24 11:34:23 -0300

    Terminando exercicio 8 da pi0007

--- src/Eduardo/PI-P0007/exercicios/exercicio08/exercicio08.cpp
+++ src/Eduardo/PI-P0007/exercicios/exercicio08/exercicio08.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include "../../funcoes/head_funcoes.h"
 
 using namespace std;
 
 void exercicio08()
 {
-    cout << "OLA MUNDO !!" << endl;
+    int altura;
+    
+    cout << "Digite a altura do padrao: ";
+    cin >> altura;
+
+    cout << "Padrão A:" << endl;
+    imprimePadraoA(altura);
+
+    cout << "\nPadrão B:" << endl;
+    imprimePadraoB(altura);
+
+    cout << "\nPadrão C:" << endl;
+    imprimePadraoC(altura);
+
 }
