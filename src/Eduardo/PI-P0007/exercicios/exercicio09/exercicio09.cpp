3bfa3a4279e632bbe9912f5e941b05455ba93dc5
Eduardo Matheus <eduardomatheusuesc@gmail.com>
2023-09-24 11:38:07 -0300

    terminando exericio 9 pi0007

--- src/Eduardo/PI-P0007/exercicios/exercicio09/exercicio09.cpp
+++ src/Eduardo/PI-P0007/exercicios/exercicio09/exercicio09.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include "../../funcoes/head_funcoes.h"
 
 using namespace std;
 
 void exercicio09()
 {
-    cout << "OLA MUNDO !!" << endl;
+    int numero;
+
+    cout << "Digite um numero inteiro: ";
+    cin >> numero;
+
+    if (ehNumeroPerfeito(numero)) {
+        cout << numero << " eh um numero perfeito." << endl;
+    } else {
+        cout << numero << " nao eh um numero perfeito." << endl;
+    }
 }
