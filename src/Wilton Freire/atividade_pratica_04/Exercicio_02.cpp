aff56e90ec0ec433e326f47c4a23b380f3824f40
WiltonLio <wilton.lio@gmail.com>
2023-09-27 08:32:05 -0300

    update

--- src/Wilton Freire/atividade_pratica_04/Exercicio_02.cpp
+++ src/Wilton Freire/atividade_pratica_04/Exercicio_02.cpp
@@ -3,28 +3,28 @@
 int main(void)
 {
     char ch = '0';
-    std::cout << ch << "..." << (int)ch << std::endl;
+    cout << ch << "..." << (int)ch << endl;
     ch = '1';
-    std::cout << ch << "..." << (int)ch++ << std::endl;
-    std::cout << ch << "..." << (int)ch++ << std::endl;
-    std::cout << ch << "..." << (int)ch++ << std::endl;
-    std::cout << ch << "..." << (int)ch++ << std::endl;
-    std::cout << ch << "..." << (int)ch++ << std::endl;
-    std::cout << ch << "..." << (int)ch++ << std::endl;
-    std::cout << ch << "..." << (int)ch++ << std::endl;
-    std::cout << ch << "..." << (int)ch++ << std::endl;
-    std::cout << ch << "..." << (int)ch << std::endl;
+    cout << ch << "..." << (int)ch++ << endl;
+    cout << ch << "..." << (int)ch++ << endl;
+    cout << ch << "..." << (int)ch++ << endl;
+    cout << ch << "..." << (int)ch++ << endl;
+    cout << ch << "..." << (int)ch++ << endl;
+    cout << ch << "..." << (int)ch++ << endl;
+    cout << ch << "..." << (int)ch++ << endl;
+    cout << ch << "..." << (int)ch++ << endl;
+    cout << ch << "..." << (int)ch << endl;
 
     int j;
     2 + 2;
-    std::cout << "entre um valor inteiro: " << std::endl;
-    std::cin >> j;
-    (j % 2 == 0) ? std::cout << "par" << std::endl : std::cout << "impar" << std::endl;
+    cout << "entre um valor inteiro: " << endl;
+    cin >> j;
+    (j % 2 == 0) ? cout << "par" << endl : cout << "impar" << endl;
     ch = (ch == '0') ? '9' : (ch == '9') ? '0' : ch;
     if (ch == '0')
     {
         ch = '9';
-        std::cout << "O caracter ch não pode ser 0" << std::endl;
+        cout << "O caracter ch não pode ser 0" << endl;
 
     }
     
