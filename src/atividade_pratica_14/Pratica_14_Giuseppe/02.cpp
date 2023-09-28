1824a1e64b828890c519696d7c3bf688860312cb
giuseppemota <75266680+giuseppemota@users.noreply.github.com>
2023-09-27 21:16:20 -0300

    Adicionando resolução P14

---
+++ src/atividade_pratica_14/Pratica_14_Giuseppe/02.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// Função para converter Celsius para Fahrenheit
+double converterParaFahrenheit(double temperaturaCelsius) {
+    return (temperaturaCelsius * 9.0 / 5.0) + 32.0;
+}
+
+// Função para solicitar a temperatura em Celsius e exibir o resultado em Fahrenheit
+void entradaESaida() {
+    double temperaturaCelsius;
+
+    // Solicita ao usuário que digite a temperatura em Celsius
+    cout << "Digite a temperatura em Celsius: ";
+    cin >> temperaturaCelsius;
+
+    // Chama a função de conversão e exibe o resultado
+    double temperaturaFahrenheit = converterParaFahrenheit(temperaturaCelsius);
+    cout << "A temperatura em Fahrenheit e: " << temperaturaFahrenheit << " F" << endl;
+}
+
+int main() {
+    // Teste a função de conversão com diferentes valores
+    entradaESaida();
+    entradaESaida();
+    entradaESaida();
+
+    return 0;
+}
