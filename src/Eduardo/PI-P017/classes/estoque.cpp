1ba4a6fc49f52da0cad7161a84b7247ffcb3cea4
Eduardo Matheus <eduardomatheusuesc@gmail.com>
2023-10-02 17:57:41 -0300

    correcao ortografica

--- src/Eduardo/PI-P017/classes/estoque.cpp
+++ src/Eduardo/PI-P017/classes/estoque.cpp
@@ -30,31 +30,3 @@ void Estoque::atualizarEstoque(int codigoProduto)
   }
 }
 
-// class Estoque
-// {
-//      private :
-//         string nome;
-//         int quantidade;
-//      public :
-
-//           string getNome(){
-//             this->nome;
-//           }
-
-//           int getQuantidae(){
-//             this->quantidade;
-//           }
-
-//           void setNome(string nome){
-//             this->nome = nome;
-//           }
-
-//           void setQuantidade(int quantidade){
-//             this->quantidade = quantidade;
-//           }
-//           void atuaizarEstoque(int codigoProduto) {
-//             if(this->quantidade > 0){
-//                 this->quantidade--;
-//             }
-//         }
-// };
