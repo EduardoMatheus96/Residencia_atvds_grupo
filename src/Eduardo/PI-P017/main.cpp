77c148089de89c7a6b43fe26ce973ac630bd3cc4
Eduardo Matheus <eduardomatheusuesc@gmail.com>
2023-10-04 19:59:16 -0300

    retirando comentarios

--- src/Eduardo/PI-P017/main.cpp
+++ src/Eduardo/PI-P017/main.cpp
@@ -11,23 +11,7 @@ int main(void)
     Produto produto1("Maca", 2.5);
     Produto produto2("Arroz", 10.00);
     Produto produto3("Leite", 4.0);
-
-    // // Exibindo informações dos produtos
-    // cout << "Produto 1:\nNome: " 
-    // << produto1.getNome() << "\nValor: " 
-    // << produto1.getValor() << "\nCódigo: " 
-    // << produto1.getCodigo() << "\n\n";
     
-    // cout << "Produto 2:\nNome: " 
-    // << produto2.getNome() << "\nValor: " 
-    // << produto2.getValor() << "\nCódigo: " 
-    // << produto2.getCodigo() << "\n\n";
-    
-    // cout << "Produto 3:\nNome: " 
-    // << produto3.getNome() << "\nValor: " 
-    // << produto3.getValor() << "\nCódigo: " 
-    // << produto3.getCodigo() << "\n";
-
     CarrinhoDeCompras carrinho;
     carrinho.adicionarProduto(produto1, 3);
     carrinho.adicionarProduto(produto2, 2);
