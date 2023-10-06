12782c56855746fc9f5f09a0393fdc017525c16f
WiltonLio <wilton.lio@gmail.com>
2023-10-06 09:56:58 -0300

    codigos da classe veiculo

---
+++ src/tic_car_rental/class_veiculo.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Veiculo
+{
+private:
+    string placa;
+    string marca;
+    string modelo;
+    int ano;
+    float valor_diaria;
+
+public:
+    void setPlaca(string placa)
+    {
+        this->placa = placa;
+    };
+
+    void setMarca(string marca)
+    {
+        this->marca = marca;
+    };
+
+    void setModelo(string modelo)
+    {
+        this->modelo = modelo;
+    };
+
+    void setAno(int ano)
+    {
+        this->ano = ano;
+    };
+
+    void setValorDiaria(float valor_diaria)
+    {
+        this->valor_diaria = valor_diaria;
+    };
+    
+    string getPlaca()
+    {
+        return this->placa;
+    };
+
+    string getMarca()
+    {
+        return this->marca;
+    };
+
+    string getModelo()
+    {
+        return this->modelo;
+    };
+
+    int getAno()
+    {
+        return this->ano;
+    };
+
+    float getValorDiaria()
+    {
+        return this->valor_diaria;
+    };
+
+};
