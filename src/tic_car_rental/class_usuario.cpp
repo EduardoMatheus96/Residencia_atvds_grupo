8656a1cc2ce1b4752871726dc5ae8ff64dd3fa6a
Matheus Nascimento <142994425+mfnswdev@users.noreply.github.com>
2023-10-06 09:36:55 -0300

    class usuario

---
+++ src/tic_car_rental/class_usuario.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+class Usuario{
+    private:
+    string cpf;
+    string nome;
+    string endereco;
+    string telefone;
+    
+    public:
+    void setCPF(string cpf){
+        this->cpf = cpf;
+    }
+
+    void setNome(string nome){
+        this->nome = nome;
+    }
+    void setEndereco(string endereco){
+        this->endereco = endereco;
+    }
+    void setTelefeone(string telefone){
+        this->telefone = telefone;
+    }
+
+    string getCPF(){
+        return cpf;
+    }
+    string getNome(){
+        return nome;
+    }
+    string getEndereco(){
+        return endereco;
+    }
+    string getTelefone(){
+        return telefone;
+    }
+};
