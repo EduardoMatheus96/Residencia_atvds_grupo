a8acd42c455f35fa24f5c009b676848398507dbd
WiltonLio <wilton.lio@gmail.com>
2023-10-10 11:37:10 -0300

    Enviando atividades 21, 22 e 25

---
+++ src/Wilton Freire/atividade_pratica_21/Exercicio_01.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Livro
+{
+public:
+    Livro(const string &titulo, const string &autor, int numCopias)
+        : titulo(titulo), autor(autor), numCopiasDisponiveis(numCopias) {}
+
+    string getTitulo() const { return titulo; }
+    string getAutor() const { return autor; }
+    int getNumCopiasDisponiveis() const { return numCopiasDisponiveis; }
+
+    void emprestarLivro()
+    {
+        if (numCopiasDisponiveis > 0)
+        {
+            numCopiasDisponiveis--;
+        }
+        else
+        {
+            cout << "Nao ha copias disponíveis deste livro." << endl;
+        }
+    }
+
+    void devolverLivro()
+    {
+        numCopiasDisponiveis++;
+    }
+
+private:
+    string titulo;
+    string autor;
+    int numCopiasDisponiveis;
+};
+
+class Usuario
+{
+public:
+    Usuario(const string &nome) : nome(nome) {}
+
+    string getNome() const { return nome; }
+
+    static void adicionarLivro(Livro livro)
+    {
+        livros.push_back(livro);
+    }
+
+    static void emprestarLivro(const string &titulo, const string &nomeUsuario)
+    {
+        for (Livro &livro : livros)
+        {
+            if (livro.getTitulo() == titulo)
+            {
+                livro.emprestarLivro();
+                emprestimos.push_back({nomeUsuario, titulo});
+                cout << "Livro emprestado com sucesso para " << nomeUsuario << "." << endl;
+                return;
+            }
+        }
+        cout << "Livro nao encontrado na biblioteca." << endl;
+    }
+
+    static void listarLivrosEmprestados(const string &nomeUsuario)
+    {
+        cout << "Livros emprestados por " << nomeUsuario << ":" << endl;
+        for (const auto &emprestimo : emprestimos)
+        {
+            if (emprestimo.nomeUsuario == nomeUsuario)
+            {
+                cout << "  - " << emprestimo.titulo << endl;
+            }
+        }
+    }
+
+private:
+    string nome;
+
+    struct Emprestimo
+    {
+        string nomeUsuario;
+        string titulo;
+    };
+
+    static vector<Livro> livros;
+    static vector<Emprestimo> emprestimos;
+};
+
+vector<Livro> Usuario::livros;
+vector<Usuario::Emprestimo> Usuario::emprestimos;
+
+int main()
+{
+    // Adicionar alguns livros à biblioteca
+    Usuario::adicionarLivro(Livro("Livro 1", "Autor 1", 5));
+    Usuario::adicionarLivro(Livro("Livro 2", "Autor 2", 3));
+    Usuario::adicionarLivro(Livro("Livro 3", "Autor 3", 2));
+
+    // Realizar alguns empréstimos
+    Usuario::emprestarLivro("Livro 1", "Usuario A");
+    Usuario::emprestarLivro("Livro 2", "Usuario B");
+    Usuario::emprestarLivro("Livro 1", "Usuario C");
+
+    // Listar livros emprestados por um usuário específico
+    Usuario::listarLivrosEmprestados("Usuario A");
+
+    return 0;
+}
---
+++ src/Wilton Freire/atividade_pratica_22/Exercicio_02.cpp
@@ -0,0 +1,191 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Tarefa
+{
+public:
+    Tarefa(const string &descricao, bool concluida = false)
+        : descricao(descricao), concluida(concluida) {}
+
+    void marcarConcluida()
+    {
+        concluida = true;
+    }
+
+    bool estaConcluida() const
+    {
+        return concluida;
+    }
+
+    string getDescricao() const
+    {
+        return descricao;
+    }
+
+private:
+    string descricao;
+    bool concluida;
+};
+
+class GerenciadorTarefas
+{
+public:
+    static void adicionarTarefa(const string &descricao)
+    {
+        tarefas.push_back(Tarefa(descricao));
+    }
+
+    static void adicionarTarefaByInstance(const Tarefa &tarefa)
+    {
+        tarefas.push_back(tarefa);
+    }
+
+    static void marcarTarefaConcluida(int indice)
+    {
+        if (indice >= 0 && indice < tarefas.size())
+        {
+            tarefas[indice].marcarConcluida();
+        }
+    }
+
+    static void listarTarefasPendentes()
+    {
+        int contador = 0;
+        for (const Tarefa &tarefa : tarefas)
+        {
+            if (!tarefa.estaConcluida())
+            {
+                cout << contador << ". " << tarefa.getDescricao() << endl;
+                contador++;
+            }
+        }
+    }
+
+    static vector<Tarefa> getTarefas()
+    {
+        return tarefas;
+    }
+
+    static void clearTarefas()
+    {
+        tarefas.clear();
+    }
+
+private:
+    static vector<Tarefa> tarefas;
+};
+
+vector<Tarefa> GerenciadorTarefas::tarefas;
+
+class BancoDeDados
+{
+public:
+    static void salvarTarefas(const string &nomeArquivo)
+    {
+        ofstream arquivo(nomeArquivo);
+        if (arquivo.is_open())
+        {
+            for (const Tarefa &tarefa : GerenciadorTarefas::getTarefas())
+            {
+                arquivo << tarefa.getDescricao() << "," << tarefa.estaConcluida() << "\n";
+            }
+            arquivo.close();
+            cout << "Tarefas salvas com sucesso." << endl;
+        }
+        else
+        {
+            cout << "Erro ao abrir o arquivo para salvar as tarefas." << endl;
+        }
+    }
+
+    static void carregarTarefas(const string &nomeArquivo)
+    {
+        ifstream arquivo(nomeArquivo);
+        if (arquivo.is_open())
+        {
+            GerenciadorTarefas::clearTarefas();
+            string linha;
+            while (getline(arquivo, linha))
+            {
+                size_t pos = linha.find(",");
+                if (pos != string::npos)
+                {
+                    string descricao = linha.substr(0, pos);
+                    bool concluida = stoi(linha.substr(pos + 1));
+                    GerenciadorTarefas::adicionarTarefaByInstance(Tarefa(descricao, concluida));
+                }
+            }
+            arquivo.close();
+            cout << "Tarefas carregadas com sucesso." << endl;
+        }
+        else
+        {
+            cout << "O arquivo de tarefas nao existe ou nao pode ser lido." << endl;
+        }
+    }
+};
+
+int main()
+{
+    BancoDeDados::carregarTarefas("tarefas.txt");
+
+    int escolha;
+    do
+    {
+        cout << "Menu:" << endl;
+        cout << "1. Adicionar tarefa" << endl;
+        cout << "2. Marcar tarefa como concluida" << endl;
+        cout << "3. Listar tarefas pendentes" << endl;
+        cout << "4. Salvar tarefas" << endl;
+        cout << "0. Sair" << endl;
+        cout << "Escolha uma opcao: ";
+        cin >> escolha;
+
+        switch (escolha)
+        {
+        case 1:
+        {
+            cin.ignore();
+            cout << "Digite a descricao da tarefa: ";
+            string descricao;
+            getline(cin, descricao);
+            GerenciadorTarefas::adicionarTarefa(descricao);
+            break;
+        }
+        case 2:
+        {
+            cout << "Digite o indice da tarefa a ser marcada como concluida: ";
+            int indice;
+            cin >> indice;
+            GerenciadorTarefas::marcarTarefaConcluida(indice);
+            break;
+        }
+        case 3:
+        {
+            GerenciadorTarefas::listarTarefasPendentes();
+            break;
+        }
+        case 4:
+        {
+            BancoDeDados::salvarTarefas("tarefas.txt");
+            break;
+        }
+        case 0:
+        {
+            cout << "Saindo do aplicativo." << endl;
+            break;
+        }
+        default:
+        {
+            cout << "Opcao invalida." << endl;
+            break;
+        }
+        }
+    } while (escolha != 0);
+
+    return 0;
+}
