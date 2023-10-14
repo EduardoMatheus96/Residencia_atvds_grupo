f0e8059f540ab29cee45242c5d79ef783ebecefd
giuseppemota <75266680+giuseppemota@users.noreply.github.com>
2023-10-13 22:36:32 -0300

    Update 01.cpp

--- src/atividade_pratica_25/Pratica_25_Giuseppe/01.cpp
+++ src/atividade_pratica_25/Pratica_25_Giuseppe/01.cpp
@@ -1,137 +1,235 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 #include <ctime>
 #include <algorithm>
 using namespace std;
 
-class Tweet {
+class Usuario; // Declaração antecipada da classe Usuario
+
+class Tweet
+{
 public:
-    Tweet(Usuario* autor, const string& conteudo)
-        : autor(autor), conteudo(conteudo) {
-        time_t now = time(0);
-        data_criacao = ctime(&now);
-    }
+    Tweet(Usuario *autor, const string &conteudo);
 
-    Usuario* getAutor() const {
-        return autor;
-    }
-
-    string getConteudo() const {
-        return conteudo;
-    }
-
-    string getDataCriacao() const {
-        return data_criacao;
-    }
+    Usuario *get_autor() const;
+    string get_conteudo() const;
+    tm get_data_criacao() const;
 
 private:
-    Usuario* autor;
+    Usuario *autor;
     string conteudo;
-    string data_criacao;
+    tm data_criacao;
 };
 
-class Usuario {
+class Usuario
+{
 public:
-    Usuario(const string& nome_usuario, const string& nome)
-        : nome_usuario(nome_usuario), nome(nome) {}
+    Usuario(const string &nome_usuario, const string &nome);
 
-    string getNomeUsuario() const {
-        return nome_usuario;
-    }
+    void postar_tweet(const string &tweet);
+    void seguir(Usuario *usuario);
+    vector<Tweet> receber_feed() const;
 
-    string getNome() const {
-        return nome;
-    }
-
-    void postarTweet(const string& conteudo) {
-        Tweet novoTweet(this, conteudo);
-        tweets.push_back(novoTweet);
-    }
-
-    void seguir(Usuario* usuario) {
-        if (usuario != this) {
-            seguindo.push_back(usuario);
-            usuario->seguidores.push_back(this);
-        }
-    }
-
-    vector<Tweet> receberFeed() const {
-        vector<Tweet> feed;
-        for (const Usuario* seguido : seguindo) {
-            for (const Tweet& tweet : seguido->tweets) {
-                feed.push_back(tweet);
-            }
-        }
-        // Ordenar os tweets em ordem cronologica reversa
-        sort(feed.begin(), feed.end(), [](const Tweet& a, const Tweet& b) {
-            return a.getDataCriacao() > b.getDataCriacao();
-        });
-        return feed;
-    }
+    string get_nome_usuario() const;
+    string get_nome() const;
+    vector<Usuario *> get_seguidores() const;
+    vector<Usuario *> get_seguindo() const;
+    vector<Tweet> get_tweets() const;
 
 private:
     string nome_usuario;
     string nome;
-    vector<Usuario*> seguidores;
-    vector<Usuario*> seguindo;
+    vector<Usuario *> seguidores;
+    vector<Usuario *> seguindo;
     vector<Tweet> tweets;
 };
 
-class RedeSocial {
-public:
-    void registrarUsuario(const string& nome_usuario, const string& nome) {
-        Usuario novoUsuario(nome_usuario, nome);
-        usuarios.push_back(novoUsuario);
-    }
-
-    Usuario* buscarUsuario(const string& nome_usuario) {
-        for (Usuario& usuario : usuarios) {
-            if (usuario.getNomeUsuario() == nome_usuario) {
-                return &usuario;
-            }
-        }
-        return nullptr;
-    }
-
-vector<Usuario> getUsuario(){
-    return usuarios;
+// Implementação de Tweet
+Tweet::Tweet(Usuario *autor, const string &conteudo)
+    : autor(autor), conteudo(conteudo)
+{
+    time_t now = time(0);
+    data_criacao = *localtime(&now);
 }
 
- void listarUsuarios(){
-    for(Usuario n : this->getUsuario())
-        cout << "Nome: " << n.getNome() << endl;      
+Usuario *Tweet::get_autor() const
+{
+    return autor;
+}
+
+string Tweet::get_conteudo() const
+{
+    return conteudo;
+}
+
+tm Tweet::get_data_criacao() const
+{
+    return data_criacao;
+}
+
+// Implementação de Usuario
+Usuario::Usuario(const string &nome_usuario, const string &nome)
+    : nome_usuario(nome_usuario), nome(nome) {}
+
+void Usuario::postar_tweet(const string &tweet)
+{
+    Tweet novo_tweet(this, tweet);
+    tweets.push_back(novo_tweet);
+}
+
+void Usuario::seguir(Usuario *usuario)
+{
+    seguindo.push_back(usuario);
+    usuario->seguidores.push_back(this);
+}
+
+// Função de comparação para objetos tm (data e hora)
+bool compare_tm(const tm &a, const tm &b)
+{
+    if (a.tm_year != b.tm_year)
+    {
+        return a.tm_year > b.tm_year;
     }
+    if (a.tm_mon != b.tm_mon)
+    {
+        return a.tm_mon > b.tm_mon;
+    }
+    if (a.tm_mday != b.tm_mday)
+    {
+        return a.tm_mday > b.tm_mday;
+    }
+    if (a.tm_hour != b.tm_hour)
+    {
+        return a.tm_hour > b.tm_hour;
+    }
+    if (a.tm_min != b.tm_min)
+    {
+        return a.tm_min > b.tm_min;
+    }
+    return a.tm_sec > b.tm_sec;
+}
+
+vector<Tweet> Usuario::receber_feed() const
+{
+    vector<Tweet> feed;
+    for (Usuario *usuario : seguindo)
+    {
+        for (const Tweet &tweet : usuario->get_tweets())
+        {
+            feed.push_back(tweet);
+        }
+    }
+
+    return feed;
+    // sort(feed.begin(), feed.end(), [](const Tweet &a, const Tweet &b)
+    //      { return compare_tm(a.get_data_criacao(), b.get_data_criacao()); });
+}
+
+string Usuario::get_nome_usuario() const
+{
+    return nome_usuario;
+}
+
+string Usuario::get_nome() const
+{
+    return nome;
+}
+
+vector<Usuario *> Usuario::get_seguidores() const
+{
+    return seguidores;
+}
+
+vector<Usuario *> Usuario::get_seguindo() const
+{
+    return seguindo;
+}
+
+vector<Tweet> Usuario::get_tweets() const
+{
+    return tweets;
+}
+
+class RedeSocial
+{
+public:
+    void registrar_usuario(const string &nome_usuario, const string &nome);
+    Usuario *buscar_usuario(const string &nome_usuario);
+    vector<Usuario> listar_usuarios() const;
+    vector<Tweet> listar_tweets(vector<Usuario> usuarios) const;
 
 private:
     vector<Usuario> usuarios;
 };
 
-int main() {
-    RedeSocial redeSocial;
+void RedeSocial::registrar_usuario(const string &nome_usuario, const string &nome)
+{
+    Usuario novo_usuario(nome_usuario, nome);
+    usuarios.push_back(novo_usuario);
+}
 
-    redeSocial.registrarUsuario("usuario1", "Usuario 1");
-    redeSocial.registrarUsuario("usuario2", "Usuario 2");
-    redeSocial.registrarUsuario("usuario3", "Usuario 3");
-
-    Usuario* usuario1 = redeSocial.buscarUsuario("usuario1");
-    Usuario* usuario2 = redeSocial.buscarUsuario("usuario2");
-    Usuario* usuario3 = redeSocial.buscarUsuario("usuario3");
-
-    redeSocial.listarUsuarios();
-
-
-    if (usuario1 && usuario2 && usuario3) {
-        usuario1->seguir(usuario2);
-        usuario1->seguir(usuario3);
-        usuario2->postarTweet("Primeiro tweet do usuario2.");
-        usuario3->postarTweet("Primeiro tweet do usuario3.");
-
-        vector<Tweet> feedUsuario1 = usuario1->receberFeed();
-        cout << "Feed do usuario1:" << endl;
-        for (const Tweet& tweet : feedUsuario1) {
-            cout << tweet.getAutor()->getNomeUsuario() << ": " << tweet.getConteudo() << endl;
+Usuario *RedeSocial::buscar_usuario(const string &nome_usuario)
+{
+    for (Usuario &usuario : usuarios)
+    {
+        if (usuario.get_nome_usuario() == nome_usuario)
+        {
+            return &usuario;
         }
+    }
+    return nullptr;
+}
+
+vector<Usuario> RedeSocial::listar_usuarios() const
+{
+    return usuarios;
+}
+
+vector<Tweet> RedeSocial::listar_tweets(vector<Usuario> usuarios) const
+{
+    vector<Tweet> todos_tweets;
+    for (Usuario &usuario : usuarios)
+    {
+        for (const Tweet &tweet : usuario.get_tweets())
+        {
+            todos_tweets.push_back(tweet);
+        }
+    }
+    return todos_tweets;
+}
+
+int main()
+{
+    // Exemplo de uso da rede social.
+    RedeSocial rede_social;
+
+    rede_social.registrar_usuario("usuario1", "Pedro");
+    rede_social.registrar_usuario("usuario2", "Joao");
+
+    Usuario *usuario1 = rede_social.buscar_usuario("usuario1");
+    Usuario *usuario2 = rede_social.buscar_usuario("usuario2");
+
+    usuario1->postar_tweet("Meu primeiro tweet!");
+    usuario2->postar_tweet("Olá, mundo!");
+
+    usuario1->seguir(usuario2);
+    usuario2->seguir(usuario1);
+    vector<Tweet> feed_usuario1 = usuario1->receber_feed();
+    vector<Tweet> feed_usuario2 = usuario2->receber_feed();
+
+    cout << "AAAAAAAAAAAAAAAA" << endl;
+
+    cout << "Feed de Usuario 1:" << endl;
+    for (const Tweet &tweet : feed_usuario1)
+    {
+        cout << tweet.get_conteudo() << endl;
+    }
+
+    cout << "Feed de Usuario 2:" << endl;
+    for (const Tweet &tweet : feed_usuario2)
+    {
+        cout << tweet.get_conteudo() << endl;
     }
 
     return 0;
